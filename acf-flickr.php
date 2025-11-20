91ba12e1233d55bc46ef4412c6bdcb36b6dcb7f0
David McDonald <info@davidmcdonald.org>
2025-11-20 12:19:58 +1100

    Remove ACF v4 support
    - Delete acf-flickr-v4.php (816 lines of unmaintained code)
    - Remove ACF v4 hook from main plugin file
    - ACF v5+ is now the minimum requirement

--- acf-flickr.php
+++ acf-flickr.php
@@ -14,17 +14,8 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 // Reference: https://codex.wordpress.org/Function_Reference/load_plugin_textdomain
 load_plugin_textdomain( 'acf-flickr', false, dirname( plugin_basename(__FILE__) ) . '/lang/' );
 
-// Include field type for ACF5
-// $version = 5 and can be ignored until ACF6 exists
+// Include field type for ACF5+
 function include_field_types_flickr( $version ) {
-  include_once('acf-flickr-v5.php');
+	include_once('acf-flickr-v5.php');
 }
 add_action('acf/include_field_types', 'include_field_types_flickr');
-
-// Include field type for ACF4
-function register_fields_flickr() {
-  include_once('acf-flickr-v4.php');
-}
-add_action('acf/register_fields', 'register_fields_flickr');
-
-?>
